dc33c7bd6c2ee5b080cd5e5313de088443c1a8d3
LAPTOP-RUA2NQ91\j-rut <josephine.rutten@fresh-minds.nl>
2021-12-01 23:06:56 +0100

    improve the code, removed one function

--- Day1/main.py
+++ Day1/main.py
@@ -1,33 +1,18 @@
-import re
-
-
 def loadfile(name):
     values = []
     f = open(name, "r")
     for x in f:
         values.append(int(x))
-    f.close()
     return values
 
-def day1a ():
+def day1 (space):
     counter = 0
-    for i in range(1, len(values)):
-        if values[i - 1] < values[i]:
-            counter += 1
-    return counter
-
-
-
-def day1b():
-    counter = 0
-    for i in range(0, len(values) - 3):
-        windowA = values[i] + values[i + 1] + values[i + 2]
-        windowB = values[i + 1] + values[i + 2] + values[i + 3]
-        if windowA < windowB:
+    for i in range(0, len(values) - space):
+        if values[i] < values[i + space]:
             counter += 1
     return counter
 
 values = loadfile("data.txt")
 print(values)
-print("solution day1a: " + str(day1a()))
-print("solution day1b: " + str(day1b()))
+print("solution day1a: " + str(day1(1)))
+print("solution day1b: " + str(day1(3)))
